99216bf1acb63a6ca5e7adf488c5997169b2e1e7
Daniel Choo <danielchoo@Daniels-MacBook-Pro-68.local>
2023-01-08 13:03:35 -0500

    Add MATH 525 Notes
    Originally, I wanted to add a notes tab on the page and
    list out all my notes there but I wasn't able to get the context
    of it with Hakyll and it ended up giving me a bunch of headaches. So,
    for now I'll just store everything on the homepage!

--- site.hs
+++ site.hs
@@ -30,16 +30,22 @@ main = hakyllWith config $ do
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
+            
+    match "notes/*.pdf" $ do
+        route idRoute
+        compile copyFileCompiler
+        
 
     create ["archive.html"] $ do
         route idRoute
         compile $ do
             posts <- recentFirst =<< loadAll "posts/*"
+                    
             let archiveCtx =
                     listField "posts" postCtx (return posts) `mappend`
                     constField "title" "Archives"            `mappend`
                     defaultContext
-
+                    
             makeItem ""
                 >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
                 >>= loadAndApplyTemplate "templates/default.html" archiveCtx
@@ -67,3 +73,5 @@ postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
+
+  
